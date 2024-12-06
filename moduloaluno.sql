8e04eeb07d3afd563d0b388f26a1057313916efa
BiancaCarolini <borazobianca@gmail.com>
2024-12-06 00:02:25 -0300

    Update moduloaluno.sql

--- moduloaluno.sql
+++ moduloaluno.sql
@@ -26,7 +26,7 @@ CREATE DATABASE IF NOT EXISTS ppa_bdd;
     inscrito BOOLEAN NOT NULL
   );
 
-  INSERT IGNORE INTO Usuarios (usuario, nome, endereco, telefone, senha, email, acesso_poder) 
+/*  INSERT IGNORE INTO Usuarios (usuario, nome, endereco, telefone, senha, email, acesso_poder) 
   VALUES ('BV1111111', 'Adm', 'Administração', '8638542954', 'adm', 'adm@gmail.com', true), 
   ('BV0000000', 'Aluno', 'Alunação', '7724114565', 'aluno', 'aluno@gmail.com', false);
   INSERT INTO Prontuarios (usuario, inscrito) VALUES
@@ -40,28 +40,162 @@ CREATE DATABASE IF NOT EXISTS ppa_bdd;
     ('BV7654321', FALSE),
     ('BV3456789', FALSE),
     ('BV1122334', FALSE);
-    
+   */ 
   CREATE TABLE IF NOT EXISTS Inscricao (
-	curso VARCHAR(200) NOT NULL, 
-	numero_pessoas_residencia INT NOT NULL, 
-	comprovante_renda_bruta_familiar VARCHAR(200)  NOT NULL, 
-	situacao_residencia_aluno  VARCHAR(200)  NOT NULL, 
-	programa_social_beneficiado  VARCHAR(200)  NOT NULL, 
-	comprovante_renda VARCHAR(200)  NOT NULL, 
-	comprovante_residencia VARCHAR(200)  NOT NULL, 
-	numero_agencia VARCHAR(6)  NOT NULL, 
-	numero_conta VARCHAR(20)  NOT NULL, 
-	imagem_RG VARCHAR(200)  NOT NULL, 
-	imagem_CPF VARCHAR(200)  NOT NULL, 
-	telefone VARCHAR(14)  NOT NULL, 
-	RG_moradores_residencia VARCHAR(200)  NOT NULL, 
+	curso VARCHAR(200), 
+	numero_pessoas_residencia INT, 
+	situacao_residencia_aluno  VARCHAR(200),
+
+	programa_social_BF  VARCHAR(200), 
+  programa_social_LOAS  VARCHAR(200),
+  programa_social_AR  VARCHAR(200),
+  programa_social_JA  VARCHAR(200),
+  programa_social_PM  VARCHAR(200),
+  programa_social_TS  VARCHAR(200),
+
+	comprovante_renda VARCHAR(200), 
+	comprovante_residencia VARCHAR(200), 
+	numero_agencia VARCHAR(6), 
+	numero_conta VARCHAR(20), 
+	imagem_RG VARCHAR(200), 
+	imagem_CPF VARCHAR(200), 
+	telefone VARCHAR(14), 
+	RG_moradores_residencia VARCHAR(200), 
 	termo_compromisso VARCHAR(200), 
-	situacao_requerimento  VARCHAR(200)  NOT NULL, 
-	auxilios_requeridos  VARCHAR(200)  NOT NULL, 
-	ID_insricao INT NOT NULL AUTO_INCREMENT, 
-	nome VARCHAR(100)  NOT NULL , 
-	usuario VARCHAR(9) PRIMARY KEY NOT NULL,
-    CONSTRAINT fk_UsuInscricao FOREIGN KEY (nome) REFERENCES Usuarios (nome),
-    CONSTRAINT fk_UsuInscricao FOREIGN KEY (usuario) REFERENCES Usuarios (usuario),
-    CONSTRAINT fk_UsuInscricao FOREIGN KEY (telefone) REFERENCES Usuarios (telefone)
+
+	situacao_requerimento  VARCHAR(200), 
+	auxilios_alimentacao  VARCHAR(200),
+  auxilios_transporte  VARCHAR(200) ,
+  auxilios_moradia  VARCHAR(200) ,
+  auxilios_saude  VARCHAR(200) ,
+  auxilios_creche  VARCHAR(200) ,
+  auxilios_material  VARCHAR(200) ,
+   
+	ID_insricao INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+	nome VARCHAR(100), 
+	/*usuario VARCHAR(9) NOT NULL,*/
+  periodo  VARCHAR(200)
+/*CONSTRAINT fk_UsuInscricao FOREIGN KEY (nome) REFERENCES Usuarios (nome),*/
+/* CONSTRAINT fk_UsuInscricao FOREIGN KEY (usuario) REFERENCES Usuarios (usuario)*/
+/*CONSTRAINT fk_UsuInscricao FOREIGN KEY (telefone) REFERENCES Usuarios (telefone)*/
   );
+  
+  
+  
+  
+  insert into 
+  inscricao (nome,
+  curso,
+  periodo,
+  comprovante_renda,
+  numero_pessoas_residencia,
+  situacao_residencia_aluno,
+  programa_social_BF,
+  programa_social_LOAS,
+  programa_social_AR,
+  programa_social_JA,
+  programa_social_PM,
+  programa_social_TS,
+  numero_agencia,
+  numero_conta,
+  telefone,
+  situacao_requerimento,
+  auxilios_alimentacao,
+  auxilios_transporte,
+  auxilios_moradia,
+  auxilios_saude,
+  auxilios_creche,
+  auxilios_material,
+  imagem_RG,
+  imagem_CPF,
+  comprovante_residencia,
+  RG_moradores_residencia,
+  termo_compromisso) 
+    values ("h",
+    "p",
+    "p",
+    "p",
+    1,
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    
+    "p",
+    "p",
+    "p",
+    "p",
+    "p");
+    
+    
+    
+    insert into 
+    inscricao(
+    nome,
+    curso,
+    periodo,
+    comprovante_renda,
+    numero_pessoas_residencia,
+    situacao_residencia_aluno,
+    programa_social_BF, programa_social_LOAS,
+    programa_social_AR, programa_social_JA,
+    programa_social_PM, programa_social_TS,
+    numero_agencia,
+    numero_conta,
+    telefone,
+    situacao_requerimento,
+    auxilios_alimentacao,
+    auxilios_transporte,
+    auxilios_moradia,
+    auxilios_saude,
+    auxilios_creche,
+    auxilios_material,
+    imagem_RG,
+    imagem_CPF,
+    comprovante_residencia,
+    RG_moradores_residencia,
+    termo_compromisso) 
+    values 
+    ("h",
+    "p",
+    "p",
+    "p",
+    1,
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    
+    "p",
+    "p",
+    "p",
+    "p",
+    "p");
+    
+    select * from inscricao;
